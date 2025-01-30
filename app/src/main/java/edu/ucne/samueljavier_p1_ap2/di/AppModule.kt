9f639857ec802de23e2ed68c7980663f640247f8
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-01-29 20:18:14 -0400

    Configurando Hilt

---
+++ app/src/main/java/edu/ucne/samueljavier_p1_ap2/di/AppModule.kt
@@ -0,0 +1,17 @@
+package edu.ucne.samueljavier_p1_ap2.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideContext(@ApplicationContext appContext: Context) = appContext
+}
