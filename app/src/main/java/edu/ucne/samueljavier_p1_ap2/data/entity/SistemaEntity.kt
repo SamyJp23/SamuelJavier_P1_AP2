e900d38159e1d188df2919e446124099739ddea9
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-02-05 18:32:56 -0400

    Renombrando y creando la entidad

--- app/src/main/java/edu/ucne/samueljavier_p1_ap2/data/entity/SistemaEntity.kt
+++ app/src/main/java/edu/ucne/samueljavier_p1_ap2/data/entity/SistemaEntity.kt
@@ -1,5 +1,12 @@
 package edu.ucne.samueljavier_p1_ap2.data.entity
 
-class Entity{
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-}
+@Entity(tableName = "Sistemas")
+data class SistemaEntity(
+    @PrimaryKey
+    val sistemaId: Int? = null,
+    val nombre: String = ""
+
+)
--- app/src/main/java/edu/ucne/samueljavier_p1_ap2/presentation/entidad/SistemaViewModel.kt
+++ app/src/main/java/edu/ucne/samueljavier_p1_ap2/presentation/entidad/SistemaViewModel.kt
@@ -2,17 +2,17 @@ package edu.ucne.samueljavier_p1_ap2.presentation.entidad
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import edu.ucne.samueljavier_p1_ap2.data.local.dao.Dao
+import edu.ucne.samueljavier_p1_ap2.data.local.dao.SistemaDao
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 @HiltViewModel
 class EntidadViewModel @Inject constructor(
-    private val dao: Dao) : ViewModel(){
-    private val _uiState = MutableStateFlow(EntidadUiState())
+    private val dao: SistemaDao) : ViewModel(){
+    private val _uiState = MutableStateFlow(SistemaUiState())
     val uiState = _uiState.asStateFlow()
 }
-fun EntidadUiState
+fun SistemaUiState
 {
 
 }
