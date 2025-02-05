5a68e13d421be312170462cc78ec15bf0fa9af6a
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-02-05 18:39:01 -0400

    Creando el Dao

--- app/src/main/java/edu/ucne/samueljavier_p1_ap2/data/local/dao/SistemaDao.kt
+++ app/src/main/java/edu/ucne/samueljavier_p1_ap2/data/local/dao/SistemaDao.kt
@@ -1,5 +1,33 @@
 package edu.ucne.samueljavier_p1_ap2.data.local.dao
 
 
-interface SistemaDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.samueljavier_p1_ap2.data.entity.SistemaEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface Dao {
+
+    @Upsert()
+    suspend fun save(sistema: SistemaEntity)
+
+    @Query(
+        """
+        SELECT * 
+        FROM Sistemas 
+        WHERE sistemaId=:id  
+        LIMIT 1
+        """
+    )
+    suspend fun find(id: Int): SistemaEntity?
+
+    @Delete
+    suspend fun delete(entidad: SistemaEntity)
+
+    @Query("SELECT * FROM Sistemas")
+    fun getAll(): Flow<List<SistemaEntity>>
+
 }
