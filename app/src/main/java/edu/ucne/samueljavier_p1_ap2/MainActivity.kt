2141d52618a1c6e40c5b9510c4a661bfc6e53da1
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-02-05 18:19:10 -0400

    Comit para iniciar el parcial

--- app/src/main/java/edu/ucne/samueljavier_p1_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/samueljavier_p1_ap2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,20 +13,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
+import edu.ucne.samueljavier_p1_ap2.presentation.entidad.EntidadViewModel
 import edu.ucne.samueljavier_p1_ap2.presentation.navigation.AppNavHost
 import edu.ucne.samueljavier_p1_ap2.ui.theme.SamuelJavier_P1_AP2Theme
+import javax.inject.Inject
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
             SamuelJavier_P1_AP2Theme {
      val navHost = rememberNavController()
                 AppNavHost(navHost)
             }
         }
     }
+
+
 }
 
 
