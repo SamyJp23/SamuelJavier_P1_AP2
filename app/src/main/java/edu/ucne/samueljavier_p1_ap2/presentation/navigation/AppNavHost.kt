460f449effeb0ba6072ee67fdb5f98ada57da0aa
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-02-05 20:07:24 -0400

    Agregando la ruta de SistemaListScreen y SistemaScreen

--- app/src/main/java/edu/ucne/samueljavier_p1_ap2/presentation/navigation/AppNavHost.kt
+++ app/src/main/java/edu/ucne/samueljavier_p1_ap2/presentation/navigation/AppNavHost.kt
@@ -1,9 +1,12 @@
 package edu.ucne.samueljavier_p1_ap2.presentation.navigation
 
+import SistemaScreen
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import edu.ucne.samueljavier_p1_ap2.presentation.entidad.SistemaListScreen
 import edu.ucne.samueljavier_p1_ap2.presentation.home.Home
 
 @Composable
@@ -21,5 +24,23 @@ fun AppNavHost(
                 }
             )
         }
+        composable<Screen.SistemaListScreen> {
+            SistemaListScreen(
+                createSistema = { navHostController.navigate(Screen.SistemaScreen(0)) },
+                goToMenu = { navHostController.navigateUp() },
+                goToSistema = { sistemaId ->
+                    navHostController.navigate(Screen.SistemaScreen(sistemaId = sistemaId))
+                }
+            )
+        }
+        composable<Screen.SistemaScreen> {
+            val sistemaId = it.toRoute<Screen.SistemaScreen>().sistemaId
+            SistemaScreen(
+                sistemaId = sistemaId,
+                goBackToList = {navHostController.navigateUp()}
+
+            )
+        }
+
     }
 }
