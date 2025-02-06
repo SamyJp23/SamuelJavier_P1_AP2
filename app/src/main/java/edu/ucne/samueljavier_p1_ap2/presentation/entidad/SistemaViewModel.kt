27fa41055e5ffbc8276a5c991553f29762ef6c49
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-02-05 20:08:33 -0400

    Trabajando el ViewModel

--- app/src/main/java/edu/ucne/samueljavier_p1_ap2/presentation/entidad/SistemaViewModel.kt
+++ app/src/main/java/edu/ucne/samueljavier_p1_ap2/presentation/entidad/SistemaViewModel.kt
@@ -1,18 +1,97 @@
 package edu.ucne.samueljavier_p1_ap2.presentation.entidad
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import edu.ucne.samueljavier_p1_ap2.data.local.dao.SistemaDao
+import edu.ucne.samueljavier_p1_ap2.data.entity.SistemaEntity
+import edu.ucne.samueljavier_p1_ap2.data.repository.SistemaRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+
 @HiltViewModel
-class EntidadViewModel @Inject constructor(
-    private val dao: SistemaDao) : ViewModel(){
+class SistemaViewModel @Inject constructor(
+    private val sistemaRepository: SistemaRepository) : ViewModel(){
     private val _uiState = MutableStateFlow(SistemaUiState())
     val uiState = _uiState.asStateFlow()
-}
-fun SistemaUiState
-{
 
+    init{
+        getEntidades()
+    }
+    fun save(){
+        viewModelScope.launch {
+            if(isValid()){
+                sistemaRepository.save(_uiState.value.toEntity())
+            }
+        }
+    }
+    fun onNombreChange(nombre: String){
+        _uiState.update {
+            it.copy(
+                nombre = nombre,
+                errorMessage = if(nombre.isBlank()) "Campo obligatorio"
+                else null
+            )
+        }
+    }
+
+
+    fun new(){
+        _uiState.value = SistemaUiState()
+    }
+    fun delete(){
+        viewModelScope.launch {
+            sistemaRepository.delete(_uiState.value.toEntity())
+        }
+    }
+    private fun getEntidades(){
+        viewModelScope.launch {
+            sistemaRepository.getAll().collect{ sistemas ->
+                _uiState.update {
+                    it.copy(sistemas = sistemas)
+                }
+            }
+        }
+    }
+
+    fun find(sistemaId: Int){
+        viewModelScope.launch {
+            if (sistemaId > 0) {
+                val sistema = sistemaRepository.find(sistemaId)
+                if (sistema != null) {
+                    _uiState.update {
+                        it.copy(
+                            sistemaId = sistema.sistemaId,
+                            nombre = sistema.nombre
+
+                        )
+                    }
+                }
+            }
+        }
+    }
+    fun isValid(): Boolean {
+        val nombreValid = _uiState.value.nombre.isNotBlank()
+
+
+        _uiState.update {
+            it.copy(
+                errorMessage = when {
+                    !nombreValid -> "Debes rellenar el campo nombre"
+                    else -> null
+                }
+            )
+        }
+
+        return nombreValid
+    }
 }
+fun SistemaUiState.toEntity() = SistemaEntity(
+    sistemaId = this.sistemaId,
+    nombre = this.nombre
+
+)
+
+
